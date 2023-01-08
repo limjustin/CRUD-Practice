695935c7896b17aaeaa705cbb9d0cad966636afe
Jaeyoung Lim <limjyjustin@gmail.com>
2023-01-08 13:25:36 +0900

    Add files via upload

---
+++ src/test/java/com/limjustin/crudprac/CrudPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.limjustin.crudprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
